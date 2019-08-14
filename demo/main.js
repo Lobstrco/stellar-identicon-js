7ad15803769d3a68abec405c8c979dd03b1fd6ad
hidden-pond <>
2019-08-14 09:16:52 +0300

    Updated demo

--- demo/main.js
+++ demo/main.js
@@ -1,14 +1,55 @@
-var randomAddressButton = document.getElementById('random-address');
+var randomAddressButton = document.getElementById('random-address'),
+    stellarAddressInput = document.getElementsByName('stellar_address')[0];
 
-randomAddressButton.onclick = function () {
-    var address = StellarSdk.Keypair.random().publicKey();
+function updateIcons (address) {
+    stellarAddressInput.value = address;
+
     var image = document.getElementById('image');
     image.src = "https://id.lobstr.co/" + address + ".png";
 
-    var icon = createStellarIdenticon(address);
-    document.getElementById('generated-canvas').innerHTML = "";
-    document.getElementsByClassName('stellar-identicon')[0].append(icon);
+    drawIdenticon(document.getElementById('existing-canvas'), address);
+}
 
-    drawIdenticon(document.getElementById('existed-canvas'), address);
+randomAddressButton.onclick = function () {
+    var address = StellarSdk.Keypair.random().publicKey();
+    updateIcons(address);
 };
 randomAddressButton.onclick();
+
+var stellarAddressChangeHandler = function () {
+    try {
+        StellarSdk.Keypair.fromPublicKey(this.value);
+        updateIcons(this.value);
+    } catch (e) {
+        console.log("Invalid stellar address");
+    }
+};
+
+
+stellarAddressInput.onchange = function () {
+    try {
+        StellarSdk.Keypair.fromPublicKey(this.value);
+        updateIcons(this.value);
+        this.parentNode.classList.remove("has-error");
+    } catch (e) {
+        this.parentNode.classList.add("has-error");
+    }
+};
+
+var timerId = null;
+stellarAddressInput.oninput = function () {
+    this.parentNode.classList.remove("has-error");
+    if (timerId) {
+        clearTimeout(timerId);
+    }
+
+    try {
+        StellarSdk.Keypair.fromPublicKey(this.value);
+        updateIcons(this.value);
+    } catch (e) {
+        var that = this;
+        timerId = setTimeout(function () {
+            that.parentNode.classList.add("has-error")
+        }, 2000);
+    }
+};
